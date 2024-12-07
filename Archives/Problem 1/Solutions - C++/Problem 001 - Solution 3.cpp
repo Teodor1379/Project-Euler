cf80a75449d593ece19875d253b0c8924b243df2
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-07 15:30:43 +0200

    Commit: Archives - Problem 1

---
+++ Archives/Problem 1/Solutions - C++/Problem 001 - Solution 3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+int main() {
+    std::cout << "The sum of the multiples of 3 and 5 below 10      is: "   << std::endl;
+    std::cout << "The sum of the multiples of 3 and 5 below 1000    is: "   << std::endl;
+
+    return 0;
+}
---
+++ Archives/Problem 1/Solutions - C++/Problem 001 - Solution 4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+int main() {
+    std::cout << "The multiples of 3 and 5 below 10     is: "   << std::endl;
+    std::cout << "The multiples of 3 and 5 below 1000   is: "   << std::endl;
+
+    return 0;
+}
