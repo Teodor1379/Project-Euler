5b0cb6fbf7089bba0b98319e1fa153d6ea1eb451
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-07 14:54:30 +0200

    Commit: Archives - Problem 1

---
+++ Archives/Problem 1/Solutions - C/Problem 001 - Solution 7.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main() {
+    printf("The sum of the multiples of 3 and 5 below 10    is: ");
+    printf("The sum of the multiples of 3 and 5 below 1000  is: " );
+
+    return 0;
+}
